3ff0e922a1db3fba24cd0478bc25287d7b49a3cf
Athul Baby <37255301+rivetin@users.noreply.github.com>
2020-10-31 23:29:16 +0530

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,3 @@
+session_start();
+unset($_SESSION["nome"]);  // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
+header("Location: home.php");
